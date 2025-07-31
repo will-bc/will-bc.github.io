a66f382bbc60c6fc1700ce8538534567add640b2
will-bc <willianbarelac@gmail.com>
2025-07-31 20:02:07 +0000

    Deploying to gh-pages from @ will-bc/will-bc.github.io@a2de474258b4461a17c3721e4e2adff63ee774c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,21 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/people/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/al-folio/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/al-folio/blog/";
+              },
+            },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
